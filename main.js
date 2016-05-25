413f8cb98d36f16bce324eb1928c8fe8b67c83c6
StefanPavel <stefan.pavel@omd.com>
2016-05-25 11:31:27 +0300

    canvas

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function deviceMoved(){
-    alert("ok");
+var value = 0;
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+function deviceMoved() {
+  value = value + 5;
+  if (value > 255) {
+    value = 0;
+  }
 }
