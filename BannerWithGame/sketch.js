5909e6f0eabe1d0b8b95e51d0a1f4b3f05a242cc
StefanPavel <stefan.pavel@omd.com>
2016-06-06 13:35:28 +0300

    new game fianished

--- BannerWithGame/sketch.js
+++ BannerWithGame/sketch.js
@@ -3,5 +3,26 @@ function setup() {
 }
 
 function draw() {
-  background(255, 0, 0);
+  background(190, 206, 212);
+  for(var i = 0; i < 5; i++){
+  	premii[i].gravity();
+  	premii[i].desen();
+  }
+  generateNewObjects();
+    drawScoreBar();
+}
+
+function mouseClicked(){
+	for(var i = 0; i < 5; i++){
+        premii[i].click();
+    }
+}
+
+function drawScoreBar(){
+    fill(0);
+    rect(0, 0, 300, 15);
+    fill(255);
+    text("Scor: " + Points, 120, 0, 300, 250);
+    noFill();
+    
 }
